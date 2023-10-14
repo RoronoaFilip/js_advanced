5c8a9757ebcbd101d66b0661e6ffe20afb47f4a7
Filip Filchev <filip.filchev@paysafe.com>
2023-10-14 22:58:53 +0300

    [other] Add more random stuff

--- random/random.js
+++ random/random.js
@@ -3,3 +3,23 @@ let a = [1,2,3];
 let b = a.slice.bind([4,5,6]);
 
 console.log(b.apply([7,8,9]));
+
+// ===================================
+
+function getFunc() {
+    var i = this.i || 0;
+
+    return function func() {
+        [].slice.call(arguments).forEach(arg => { i += +arg });
+        [...arguments].forEach(arg => { i += +arg });
+        return i;
+    }
+}
+
+var obj = {
+    i: 0
+}
+
+var func = getFunc.apply(obj);
+
+console.log(func.call(null, 1,2,3));
