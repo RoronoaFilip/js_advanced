f2d6ef879db1597401d3bc9dd9539ee50259c334
Filip Filchev <fkfilchev@gmail.com>
2023-10-21 15:10:15 +0300

    [labs2] Add Removing of Duplicates

--- labs/lab2/filter.js
+++ labs/lab2/filter.js
@@ -1,3 +1,17 @@
+function removeDuplicates() {
+    var array = arguments[0] || this;
+    var uniqueArray = [];
+
+    for (const i in array) {
+        if (uniqueArray.indexOf(array[i]) === -1) {
+            uniqueArray.push(array[i]);
+        }
+    }
+
+    return uniqueArray;
+}
+
+
 function myFilter(array, predicate) {
     var array1 = [];
     var array2 = [];
@@ -13,4 +27,6 @@ function myFilter(array, predicate) {
     return [array1, array2];
 }
 
-console.log(myFilter([1, 2, 3, 4, 5, 6, 7, 8, 9, 312, 5, 4325, 2314, 1234123], x => !!(x % 2)));
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 312, 5, 4325, 2314, 1234123];
+
+console.log(myFilter(removeDuplicates.call(arr), x => !!(x % 2)));
